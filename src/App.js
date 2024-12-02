e465cdcbfb9d7ea9a9970f3cfd68258f90e28b4c
sreejitha <sreejitha@webandcrafts.in>
2024-12-02 09:32:48 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import MainContainer from './components/MainContainer/MainContainer';
 import TabBar from './components/TabBar/TabBar';
 
 function App() {
+  useEffect(() => {
+    const handleResize = () => {
+      window.location.reload();
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  
   return <Layout>
      <TabBar/>
      <MainContainer/>
